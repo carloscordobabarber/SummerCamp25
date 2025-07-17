9c3af1a69325cf1bf0f9fc6291a3eeb2f6084cbf
carlos <carlosernestocordobabarber@gmail.com>
2025-07-17 09:37:49 +0200

    Cambio de ejercicio, Selector dia semanal

--- HolaMundoSolucion/HolaMundoProyecto/Program.cs
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+String entrada;
+
+do {
+    Console.WriteLine("Elija un numero del 1 al 7, introduzca 0 para salir");
+     entrada = Console.ReadLine();
+    
+    switch (entrada) {
+        case "1":
+            Console.WriteLine("Lunes");
+            break;
+        case "2":
+            Console.WriteLine("Martes");
+            break;
+        case "3":
+            Console.WriteLine("Miercoles");
+            break;
+        case "4":
+            Console.WriteLine("Jueves");
+            break;
+        case "5":
+            Console.WriteLine("Viernes");
+            break;
+        case "6":
+            Console.WriteLine("Sabado");
+            break;
+        case "7":
+            Console.WriteLine("Domingo");
+            break;
+        default:
+            Console.WriteLine("Elija un numero valido");
+            break;
+
+    }
+} while (entrada!="0");
