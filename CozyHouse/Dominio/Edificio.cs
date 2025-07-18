41f256fc470881e1346774b9ad05d03deecc58bc
Embie Alieva <emutlueali@educacion.navarra.es>
2025-07-18 13:40:51 +0200

    He creado las clases Indicedncia y Edificio

---
+++ CozyHouse/Dominio/Edificio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Edificio
+    {
+        private string direcction;
+        private int floor;
+        private string door;
+
+        public string Direcction { get; set; }
+
+        public int Floor { get; set; }
+
+        public string Door { get; set; }
+
+
+        public Edificio() { }
+
+        public Edificio(string direcction, int floor, string door)
+        {
+            this.Direcction = direcction;
+            this.Floor = floor;
+            this.Door = door;
+        }
+
+
+    }
+}
---
+++ CozyHouse/Dominio/Incidencia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Incidencia
+    {
+        // Propiedades automáticas
+        public string OwnersName { get; set; }
+        public string ClientName { get; set; }
+        public string Direcction { get; set; }
+        public string Incidence { get; set; }
+
+        // Constructores
+        public Incidencia() { }
+
+        public Incidencia(string ownersName, string clientName, string direcction, string incidence)
+        {
+            OwnersName = ownersName;
+            ClientName = clientName;
+            Direcction = direcction;
+            Incidence = incidence;
+        }
+    }
+}
