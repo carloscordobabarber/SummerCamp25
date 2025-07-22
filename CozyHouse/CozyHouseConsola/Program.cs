ffdb2c50f1c510111003e38d9014df7e6545554e
carlos <carlosernestocordobabarber@gmail.com>
2025-07-22 13:37:52 +0200

    prueba Git

--- CozyHouse/CozyHouseConsola/Program.cs
+++ CozyHouse/CozyHouseConsola/Program.cs
@@ -3,3 +3,4 @@
 Console.WriteLine("Welcome to CozyHouseConsola!");
  Client client = new Client(1, "464798", "John Doe", "1234567890","65682");
 Console.WriteLine($"Client ID: {client.GetId()}");
+//comentario
