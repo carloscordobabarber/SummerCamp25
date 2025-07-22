85a6ed82cf2f0c398711bcdf4fea0043cb74e92f
Embie Alieva <emutlueali@educacion.navarra.es>
2025-07-22 11:26:53 +0200

    He hecho la clase Empleado.

--- ListasMantenimiento/ListasMantenimientoSolucion/Dominio/Empleado.cs
+++ ListasMantenimiento/ListasMantenimientoSolucion/Dominio/Empleado.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Empleado
-    {
+    public class Empleado {
+        public int id;
+        public string nombre;
+        public string apellido;
+        public string email;
+        public string telefono;
+        public string direccion;
+        public DateTime fechaNacimiento;
+        public string puesto;
+
+        public Empleado() { }
+
+        public Empleado(int id, string nombre, string apellido, string email, string telefono, string direccion, DateTime fechaNacimiento, string puesto) {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.email = email;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.fechaNacimiento = fechaNacimiento;
+            this.puesto = puesto;
+        }
+
+        public override string ToString() {
+            return $"{id} - {nombre} {apellido} - {email} - {telefono} - {direccion} - {fechaNacimiento.ToShortDateString()} - {puesto}";
+        }
+
+
     }
 }
