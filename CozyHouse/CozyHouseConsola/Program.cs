9df37778badecc3761951d5c0bb6aa6e4a7a8b68
carlos <carlosernestocordobabarber@gmail.com>
2025-07-22 13:04:41 +0200

    Actualizacion Summer

---
+++ CozyHouse/CozyHouseConsola/Program.cs
@@ -0,0 +1,5 @@
+﻿using Dominio;
+
+Console.WriteLine("Welcome to CozyHouseConsola!");
+ Client client = new Client(1, "464798", "John Doe", "1234567890","65682");
+Console.WriteLine($"Client ID: {client.GetId()}");
