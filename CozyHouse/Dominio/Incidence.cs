a1c9c298c9e8fc9ec56502eda0df37e9f78c0a7b
Embie Alieva <emutlueali@educacion.navarra.es>
2025-07-28 14:01:14 +0200

    Cambios meneores en la clase incidencia

--- CozyHouse/Dominio/Incidence.cs
+++ CozyHouse/Dominio/Incidence.cs
@@ -13,16 +13,22 @@ namespace Dominio
         string ownersName;
         string clientName;
         string direcction;
-        string incidence;
+        DateTime creationDate;
+        DateTime resolutionDate;
+        string description;
         string status;
+        string responsable;
+        string incidencePriority;
 
 
         public int IdIncidence { get; set; }
         public string OwnersName { get; set; }
         public string ClientName { get; set; }
         public string Direcction { get; set; }
-        public string TheIncidence { get; set; }
-
+        public DateTime CreationDate { get; set; }
+        public DateTime ResolutionDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public string Description { get; set; }  
         public string Status
         {
             get { return status; }
@@ -36,24 +42,36 @@ namespace Dominio
             }
         }
 
-        public DateTime Date { get; set; }
+        public string Responsable { get; set; }
+        public Priority IncidencePriority { get; set; }
+        public enum Priority
+        {
+            Baja,
+            Media,
+            Alta,
+            Urgente
+        }
 
         public Incidence() { }
 
-        public Incidence(int idIncidence, string ownersName, string clientName, string direcction, string incidence, string status)
+        public Incidence(int idIncidence, string ownersName, string clientName, string direcction, DateTime creationDate, DateTime resolutionDate, string description, string status, string responsable, Priority incidencePriority)
         {
-            this.IdIncidence = idIncidence;
-            this.OwnersName = ownersName;
-            this.ClientName = clientName;
-            this.Direcction = direcction;
-            this.TheIncidence = incidence;
-            this.Status = status;
+            IdIncidence = idIncidence;
+            OwnersName = ownersName;
+            ClientName = clientName;
+            Direcction = direcction;
+            CreationDate = creationDate;
+            ResolutionDate = resolutionDate;
+            Description = description;
+            Status = status;
+            Responsable = responsable;
+            IncidencePriority = incidencePriority;
         }
 
         public void UpdateStatus(string newStatus) {
             if (newStatus == "Pendiente" || newStatus == "En Proceso" || newStatus == "Resuelta") {
                 this.Status = newStatus;
-                if (newStatus == "Resuelta") {  this.Date = DateTime.Now; }
+                if (newStatus == "Resuelta") {  this.LastUpdateDate = DateTime.Now; }
             } else {
                 throw new ArgumentException("El estado debe ser 'Pendiente', 'En Proceso' o 'Resuelta'.");
             }
@@ -61,21 +79,23 @@ namespace Dominio
 
         public void CloseIncidence() {
             this.Status = "Resuelta";
-            this.Date = DateTime.Now;
+            this.ResolutionDate = DateTime.Now;
         }
 
-        public void AddIncidence(int idIncidence, string ownersName, string clientName, string direcction, string incidence, string status, DateTime incidenceDate)
+        public void AddIncidence(int idIncidence, string ownersName, string clientName, string direcction, string description, string status, DateTime incidenceDate)
         {
             this.IdIncidence = idIncidence;
             this.OwnersName = ownersName;
             this.ClientName = clientName;
             this.Direcction = direcction;
-            this.TheIncidence = incidence;
+            this.Description = description;
             this.Status = status;
-            this.Date = incidenceDate;
+            this.CreationDate = incidenceDate;
         }
 
-        public void AssignTechnician(Incidence newIncidence) {
+        public void AssignTechnician(Incidence newIncidence, string responsable) {
+            newIncidence.Responsable = responsable;
+            newIncidence.LastUpdateDate = DateTime.Now;
             if (newIncidence.Status == "Pendiente") {
                 newIncidence.Status = "En Proceso";
             } else {
