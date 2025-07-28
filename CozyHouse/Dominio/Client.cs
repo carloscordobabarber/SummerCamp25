f7e8654712423c96307d1ec1595403f61f701b54
carlos <carlosernestocordobabarber@gmail.com>
2025-07-28 13:17:04 +0200

    ApisClient "Creadas"

--- CozyHouse/Dominio/Client.cs
+++ CozyHouse/Dominio/Client.cs
@@ -1,67 +1,22 @@
 ﻿using System;
-
+namespace Dominio;
 public class Client
 {
-    private int id;
-	private string dni;
-	private string name;
-	private string bankAccount;
-	private string phoneNumber;
+    public int Id { get; set; }
+    public string Dni { get; set; }
+    public string Name { get; set; }
+    public string BankAccount { get; set; }
+    public string PhoneNumber { get; set; }
 
-	public Client(int id, string dni, string name, string bankAccount, string phoneNumber)
-	{
-        this.id = id;
-        this.dni = dni;
-        this.name = name;
-        this.bankAccount = bankAccount;
-        this.phoneNumber = phoneNumber;
-    }
+    public Client() { }
 
-    // Métodos
-
-
-
-    // Getters
-    public int GetId()
+    public Client(int id, string dni, string name, string bankAccount, string phoneNumber)
     {
-        return id;
-    }
-	public string GetDni()
-    {
-        return dni;
-    }
-    public string GetName()
-    {
-        return name;
-    }
-    public string GetBankAccount()
-    {
-        return bankAccount;
-    }
-    public string GetPhoneNumber()
-    {
-        return phoneNumber;
-    }
-
-    // Setters
-    public void SetId(int id)
-    {
-        this.id = id;
-    }
-    public void SetDni(string dni)
-    {
-        this.dni = dni;
-    }
-    public void SetName(string name)
-    {
-        this.name = name;
-    }
-    public void SetBankAccount(string bankAccount)
-    {
-        this.bankAccount = bankAccount;
-    }
-    public void SetPhoneNumber(string phoneNumber)
-    {
-        this.phoneNumber = phoneNumber;
+        Id = id;
+        Dni = dni;
+        Name = name;
+        BankAccount = bankAccount;
+        PhoneNumber = phoneNumber;
     }
 }
+
---
+++ CozyHouse/SistemaAPI/Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+// Archivo: ClientController.cs
+using Microsoft.AspNetCore.Mvc;
+using Dominio;
+
+namespace SistemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        // GET api/client
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var cliente = new Client(1, "12345678A", "Juan Pérez", "ES1234567890123456789012", "612345678");
+            return Ok(cliente);
+        }
+
+        // POST api/client
+        [HttpPost]
+        public IActionResult Post([FromBody] Client client)
+        {
+            // Aquí podrías agregar lógica para guardar el cliente en una base de datos
+            return CreatedAtAction(nameof(Get), new { id = client.Id }, client);
+        }
+    }
+}
