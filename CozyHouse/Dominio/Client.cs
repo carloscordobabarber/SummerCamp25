653b0550c53784b6b1ddefdd732e7cae1bf263f1
Embie Alieva <emutlueali@educacion.navarra.es>
2025-07-29 14:08:36 +0200

    He nodificado apartados repetidos en Clintes

--- CozyHouse/Dominio/Client.cs
+++ CozyHouse/Dominio/Client.cs
@@ -6,13 +6,11 @@ public class Client
     public int Id { get; set; }
     public string Dni { get; set; }
     public string Name { get; set; }
-    public DateTime BirthDate { get; set; }
     public string BankAccount { get; set; }
     public string PhoneNumber { get; set; }
     public string Email { get; set; } = string.Empty;
 
     public DateTime Birthday { get; set; }
-    public string Email { get; set; }
 
     public Client() { }
 
@@ -32,7 +30,6 @@ public class Client
         Id = id;
         Dni = dni;
         Name = name;
-        BirthDate = dateOfBirth;
         BankAccount = bankAccount;
         PhoneNumber = phoneNumber;
         Email = email;
