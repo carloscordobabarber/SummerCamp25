eb49e918391b18ac371e042320b74f0003652853
EmbieAlieva <emutlueali@educacion.navarra.es>
2025-08-19 11:19:01 +0200

    he añadido un servicio para que cors no pete

--- CozyHouse/SistemaAPI/Program.cs
+++ CozyHouse/SistemaAPI/Program.cs
@@ -12,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// CORS: permite solicitudes desde Angular (localhost:4200)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.AllowAnyOrigin() //WithOrigins("http://localhost:4200") // Cambia si usas otra URL
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 // Configurar AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 // Agregar el contexto de base de datos
