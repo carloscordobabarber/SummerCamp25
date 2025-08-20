afbe4d38d848f64be6a573b02e92c6b35ccc65e1
JavierPeraltaAzagra <javierpe12345@gmail.com>
2025-08-20 12:25:12 +0200

    Creado servicio api cliente get y post

---
+++ CozyFront/src/app/services/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'https://devdemoapi4.azurewebsites.net/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  createUser(user: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, user);
+  }
+}
